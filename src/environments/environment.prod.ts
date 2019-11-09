1073bafabd1c8a27987d7d4257304ab539f6a937
MariamMach <39934426+MariamMach@users.noreply.github.com>
2019-11-09 23:54:04 +0100

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAdaQGs7qM8q-3aB9gzbGARQ1MrO7NcrIs',
+    authDomain: 'mariam-machaallah-online-shop.firebaseapp.com',
+    databaseURL: 'https://mariam-machaallah-online-shop.firebaseio.com',
+    projectId: 'mariam-machaallah-online-shop',
+    storageBucket: 'mariam-machaallah-online-shop.appspot.com',
+    messagingSenderId: '844515797081',
+    appId: '1:844515797081:web:33438a555a67fa936c6948',
+    measurementId: 'G-10VLR44PEW'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAdaQGs7qM8q-3aB9gzbGARQ1MrO7NcrIs',
+    authDomain: 'mariam-machaallah-online-shop.firebaseapp.com',
+    databaseURL: 'https://mariam-machaallah-online-shop.firebaseio.com',
+    projectId: 'mariam-machaallah-online-shop',
+    storageBucket: 'mariam-machaallah-online-shop.appspot.com',
+    messagingSenderId: '844515797081',
+    appId: '1:844515797081:web:33438a555a67fa936c6948',
+    measurementId: 'G-10VLR44PEW'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
