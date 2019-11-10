7785a0ea4b98088ba64aafefd684e322301ff573
MariamMach <39934426+MariamMach@users.noreply.github.com>
2019-11-10 13:21:54 +0100

    implementing logout

--- src/app/my-navbar/my-navbar.component.ts
+++ src/app/my-navbar/my-navbar.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-my-navbar',
   templateUrl: './my-navbar.component.html',
   styleUrls: ['./my-navbar.component.css']
 })
-export class MyNavbarComponent implements OnInit {
+export class MyNavbarComponent {
+  user$: Observable<firebase.User>;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    this.user$ = afAuth.authState;
+  }
 
-  ngOnInit() {
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
