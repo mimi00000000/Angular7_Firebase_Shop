c2319cec8d8216631dc21acefd3b3ad1a5766948
MariamMach <39934426+MariamMach@users.noreply.github.com>
2019-11-11 17:06:13 +0100

    refresh bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,12 @@ export class AppComponent {
 
   constructor(private userService: UserService, private auth: AuthService, router: Router) {
     auth.user$.subscribe( user => {
-        if (user) {
-          userService.save(user);
-          const returnUrl = localStorage.getItem('returnUrl');
-          router.navigateByUrl(returnUrl);
-        }
+        if (!user) { return; }
+        userService.save(user);
+        const returnUrl = localStorage.getItem('returnUrl');
+        if (!returnUrl) {  return;   }
+        localStorage.removeItem('returnUrl');
+        router.navigateByUrl(returnUrl);
     });
   }
 
